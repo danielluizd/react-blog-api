14f0a5da4254807ee3d5c38848593f229742e0b4
danielluizd <daniel_cbamt@hotmail.com>
2022-07-11 11:54:52 -0400

    Inserção de uma função para permissão do 'CORS' na aplicação.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,25 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.enableCors();
   await app.listen(process.env.PORT || 3000);
+
+  var whitelist = ['http://localhost:3000', 'https://localhost:3000'];
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
+  // await app.listen(3000);
 }
 bootstrap();
